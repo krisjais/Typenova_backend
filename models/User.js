e7648e3f5394f300c927e8840e3259d53b1b7ac8
VJ <visheshjaiswar009@gmail.com>
2026-05-04 20:37:32 +0530

    pass

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = function (password) {
